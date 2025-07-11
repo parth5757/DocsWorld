d12bd3a902cc8a7ca11653bc9e306cd76ab53d5a
Parth Thakkar <psthakkar2@gmail.com>
2025-07-11 17:47:38 +0530

    django settings media url root added at urls.py

--- Backend/docs_world/docs_world/urls.py
+++ Backend/docs_world/docs_world/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from docs_world.docs_world import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("image.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
